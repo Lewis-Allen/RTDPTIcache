ecdd4237dbd2845903b68e4d5cf6889606b95173
Lewis-Allen <lraa12@brighton.ac.uk>
2018-12-09 22:32:45 +0000

    Initial commit

---
+++ src/test/java/com/lewisallen/rtdptiCache/AppTest.java
@@ -0,0 +1,14 @@
+package com.lewisallen.rtdptiCache;
+
+import org.junit.Test;
+
+/**
+ * Basic Unit Test
+ */
+public class AppTest {
+	
+	@Test
+	public void setup() {
+		System.out.println("Success");
+	}
+}
