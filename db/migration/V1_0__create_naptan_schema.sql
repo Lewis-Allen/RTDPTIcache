8049631740a8a719b8fc51f26083ae423916b699
Lewis-Allen <lraa12@brighton.ac.uk>
2018-12-15 13:26:40 +0000

    added inserts to migration

--- db/migration/V1_0__create_naptan_schema.sql
+++ db/migration/V1_0__create_naptan_schema.sql
@@ -10,3 +10,32 @@ CREATE TABLE `naptan` (
   `Northing` text,
   `Retrieve` tinyint(4) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+INSERT INTO `test`.`naptan`
+(
+	`SMS`,
+	`LongDescription`,
+	`Active`,
+	`SystemCodeNumber`,
+	`Easting`,
+	`Lat`,
+	`Lng`,
+	`Identifier`,
+	`Northing`,
+	`Retrieve`
+)
+VALUES
+(
+	'brimwtp',
+	'Varley Halls',
+	'True',
+	'149000001995',
+	'533160',
+	'50.86423',
+	'-0.10918',
+	'o/s',
+	'108895',
+	1
+);
+
+INSERT INTO naptan (SMS, LongDes) VALUES ('DeLorean');
