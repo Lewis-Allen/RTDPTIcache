22988e56528f39e86ac015726f518d1e7ca903ac
Lewis-Allen <lewisn11@hotmail.co.uk>
2018-12-18 18:04:28 +0000

    Added endpoint

--- src/main/java/com/lewisallen/rtdptiCache/api/StopController.java
+++ src/main/java/com/lewisallen/rtdptiCache/api/StopController.java
@@ -1,16 +1,37 @@
 package com.lewisallen.rtdptiCache.api;
 
 import com.lewisallen.rtdptiCache.caches.SIRICache;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class StopController {
 
-    @RequestMapping(value="api/stop", method=RequestMethod.GET)
-    public String stops(@RequestParam(value="code", required=true) String[] codes){
-        return SIRICache.getSiriJson(codes);
+    @RequestMapping(value="api/stop", method=RequestMethod.POST)
+    public ResponseEntity<String> stops(@RequestBody JSONObject json){
+        try
+        {
+            JSONArray jsonCodeList = json.getJSONArray("codes");
+            List<String> codes = new ArrayList<String>();
+            for(int i = 0; i < jsonCodeList.length(); i++){
+                codes.add(jsonCodeList.get(i).toString());
+            }
+
+            return new ResponseEntity(SIRICache.getSiriJson(codes.stream().toArray(String[]::new)),HttpStatus.OK);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return new ResponseEntity(null,HttpStatus.BAD_REQUEST);
     }
 }
---
+++ src/test/java/com/lewisallen/rtdptiCache/tests/ApiTest.java
@@ -0,0 +1,12 @@
+package com.lewisallen.rtdptiCache.tests;
+
+import org.junit.Test;
+
+public class ApiTest {
+
+    @Test
+    public void postTest(){
+        assert(true);
+    }
+
+}
