bfe14e80290480956899dabd98ce58259214309e
Lewis-Allen <lewisn11@hotmail.co.uk>
2019-02-11 17:56:15 +0000

    Configured tests for siri and trains

--- src/test/java/com/lewisallen/rtdptiCache/tests/ScheduledTaskTest.java
+++ src/test/java/com/lewisallen/rtdptiCache/tests/ScheduledTaskTest.java
@@ -1,9 +1,20 @@
 package com.lewisallen.rtdptiCache.tests;
 
+import com.lewisallen.rtdptiCache.AppConfig;
 import com.lewisallen.rtdptiCache.jobs.ScheduledTasks;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ScheduledTaskTest {
+
+    @BeforeAll
+    void setup(){
+        new AppConfig();
+    }
     
     @Test
     public void testUpdateNaPTANCache(){
@@ -15,5 +26,33 @@ public class ScheduledTaskTest {
     public void testUpdateTrainStationCache(){
         ScheduledTasks tasks = new ScheduledTasks();
         tasks.updateStationCache();
+    }
+
+    @Test
+    @DisabledIfEnvironmentVariable(named="CI", matches="true")
+    void testUpdateSIRICache(){
+        ScheduledTasks tasks = new ScheduledTasks();
+        tasks.updateSIRICache();
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named="CI", matches="true")
+    void ciTestUpdateSIRICache(){
+        AppConfig.siriUri = System.getenv("SIRI_URI");
+        testUpdateSIRICache();
+    }
+
+    @Test
+    @DisabledIfEnvironmentVariable(named="CI", matches="true")
+    void testUpdateStationsCache(){
+        ScheduledTasks tasks = new ScheduledTasks();
+        tasks.updateStationCache();
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named="CI", matches="true")
+    void ciTestUpdateStationsCache(){
+        AppConfig.ldbToken = System.getenv("LDB_TOKEN");
+        testUpdateStationsCache();
     }
 }
