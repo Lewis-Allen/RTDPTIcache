a5dbbfe1e09b2b82e2cffd32a691a1ec88d5df52
Lewis-Allen <lewisn11@hotmail.co.uk>
2019-03-05 14:22:38 +0000

    Removed application.properties dependency.

--- src/main/java/com/lewisallen/rtdptiCache/AppConfig.java
+++ src/main/java/com/lewisallen/rtdptiCache/AppConfig.java
@@ -2,13 +2,10 @@ package com.lewisallen.rtdptiCache;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("application.properties")
 public class AppConfig
 {
-
     public static String siriUri;
     public static String ldbToken;
 
